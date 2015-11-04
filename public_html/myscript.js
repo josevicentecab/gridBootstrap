7794ba07d5c29cdac951a302734f408dd8eadf04
josevicentecabanas <josevicentecabanas@10.230.169.121>
2015-11-04 17:38:39 +0000

    Form Jquery

--- public_html/myscript.js
+++ public_html/myscript.js
@@ -1,9 +1,16 @@
-$(document).ready(
-        function() {
-            $("input[type='submit']").on('click', function(){
-                var myli = "<li>" + $("#texto").val() + "</li>";
-                $("#lista").append(myli);                
-            });
-        }
-);
+$(document).ready(function() {
+        var mifuncion = function() {
+            var password1 = $("#password").val();
+            var password2 = $("#password_repeat").val();
+            if (password1 != password2) {
+                $(".correcto").fadeOut("fast");
+                $(".danger").fadeIn("slow");
+                
+            } else {
+                $(".danger").hide();
+                $(".correcto").show();
+            }
+        };
+        $("input[type='submit']").on("click", mifuncion);
+ });
 
